0fdbbf06221a1cec9a98317ca23b7fb2c0325855
Ryan Souza <147557760+ryancwb@users.noreply.github.com>
2023-10-10 18:13:11 -0300

    Create README.py
    Sobre mim

---
+++ README.py
@@ -0,0 +1,5 @@
+
+  print("Sou um desenvolvedor full stack apaixonado pela criação de soluções de software inovadoras e eficientes. 
+  Minha especialidade é a programação em Python, e estou comprometido em construir aplicativos de alta qualidade que atendam às necessidades dos usuários. 
+  Com experiência em desenvolvimento web e móvel, estou sempre em busca de desafios que me permitam aprender e crescer.")
+
